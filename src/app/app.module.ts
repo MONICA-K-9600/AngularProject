2e54a14e29874d817923985dd3ecfb2ab12b6060
HP <monicamonisa2@gmail.com>
2022-02-10 20:56:13 +0530

    begining commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ZIndexComponent } from './z-index/z-index.component';
+import { BeforeAfterComponent } from './before-after/before-after.component';
+import { OpacityComponent } from './opacity/opacity.component';
+import { TransformComponent } from './transform/transform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZIndexComponent,
+    BeforeAfterComponent,
+    OpacityComponent,
+    
+    TransformComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/before-after/before-after.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-before-after',
+  templateUrl: './before-after.component.html',
+  styleUrls: ['./before-after.component.css']
+})
+export class BeforeAfterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
